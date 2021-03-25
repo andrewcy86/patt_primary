c22dfa134783080e89c203970ac164c5ecc6e4ba
Andrew Yuen <yuen.andrew@epa.gov>
2021-03-25 16:32:04 -0400

    s3 link fix

--- includes/admin/pages/folder-file-details.php
+++ includes/admin/pages/folder-file-details.php
@@ -5,6 +5,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $wpdb, $current_user, $wpscfunction;
 
+require WPPATT_ABSPATH . 'includes/admin/pages/scripts/vendor/autoload.php';
+
+use Aws\S3\S3Client;  
+use Aws\Exception\AwsException;
+
 $subfolder_path = site_url( '', 'relative');
 include_once( WPPATT_UPLOADS . 'api_authorization_strings.php' );
 
@@ -689,10 +694,6 @@ echo '<span class="details-name" >File Size: </span><span class="" >' . $file_si
 echo 'objectkey ' . $folderfile_details->object_key;
 
 if ($folderfile_details->object_key != '') {
-
-require WPPATT_ABSPATH . 'includes/admin/pages/scripts/vendor/autoload.php';
-use Aws\S3\S3Client;  
-use Aws\Exception\AwsException;
 
 $s3Client = new Aws\S3\S3Client([
     'region' => $s3_region,
