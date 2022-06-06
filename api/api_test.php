ebba9dc066630414f7b7c639b8fdd41013f7fb76
Andrew Yuen <yuen.andrew@epa.gov>
2022-06-06 16:26:10 -0400

    API Update

--- api/api_test.php
+++ api/api_test.php
@@ -2,6 +2,14 @@
 use function Env\env;
 require '/public/data/patt/patt-approot/vendor/autoload.php';
 
+$root_dir = dirname(__DIR__);
+
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable($root_dir);
+
+if (file_exists($root_dir . '/.env')) {
+    $dotenv->load();
+}
+
 $host = env('STAGE_DB_HOST'); /* Host name */
 $user = env('STAGE_DB_USER'); /* User */
 $password = env('STAGE_DB_PASS'); /* Password */
