2c958950e59291c1698f43916520203c51349297
Andrew Yuen <yuen.andrew@epa.gov>
2022-06-06 17:05:54 -0400

    API fix

--- api/api.php
+++ api/api.php
@@ -12055,22 +12055,10 @@ namespace Tqdev\PhpCrudApi {
     use Tqdev\PhpCrudApi\RequestFactory;
     use Tqdev\PhpCrudApi\ResponseUtils;
 
-    use function Env\env;
-    require '/public/data/patt/patt-approot/vendor/autoload.php';
+    require_once '/public/data/patt/patt-approot/vendor/autoload.php';
+    require_once '/public/data/patt/patt-approot/config/application.php';
     
-    $root_dir = dirname(__DIR__);
-    
-    $dotenv = Dotenv\Dotenv::createUnsafeImmutable($root_dir);
-    
-    if (file_exists($root_dir . '/.env')) {
-        $dotenv->load();
-        $dotenv->required(['WP_HOME', 'WP_SITEURL']);
-        if (!env('DATABASE_URL')) {
-            $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD']);
-        }
-    }
-    
-    $host = DATABASE_URL; /* Host name */
+    $host = DB_HOST; /* Host name */
     $user = DB_USER; /* User */
     $password = DB_PASSWORD; /* Password */
     $dbname = DB_NAME; /* Database name */
