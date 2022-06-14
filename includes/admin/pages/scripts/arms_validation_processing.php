94656fbaaf7aef8654de5ca6c61ac025584975f8
Andrew Yuen <yuen.andrew@epa.gov>
2022-06-14 15:45:57 -0400

    Test presigned url

--- includes/admin/pages/scripts/arms_validation_processing.php
+++ includes/admin/pages/scripts/arms_validation_processing.php
@@ -34,11 +34,6 @@ require_once($_SERVER['DOCUMENT_ROOT'].$WP_PATH.'/wp/wp-load.php');
 }
 </style>
 
-<script
-  src="https://code.jquery.com/jquery-3.6.0.min.js"
-  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-  crossorigin="anonymous"></script>
-
 <?php
 
 if(isset($_POST['postvarsobjects'])){
