02e643a3700a2fd1bc1766bdb98934a5f3b70073
Andrew Yuen <yuen.andrew@epa.gov>
2023-01-03 09:31:11 -0500

    PDF Test Update to send hash to validate pdf include

--- includes/admin/pages/scripts/validate_paging.php
+++ includes/admin/pages/scripts/validate_paging.php
@@ -55,12 +55,13 @@ echo $box_status;
 echo $box_ticket_status;
 echo $folderdocinfofile_id_validation;
 
+$switch_file_hash = $folderdocinfofile_id_initial;
 
 if($folderdocinfofile_id_count == 0) {
 echo'<script type="text/javascript" id="runscript">switchFile("");switchInfo("");</script>';
 } else {  
 echo'<script type="text/javascript" id="runscript">
-switchFile(\'' . $folderdocinfofile_id_initial . '\');switchInfo(\'' . $folderdocinfofile_id_initial . '\');
+switchFile(\'' . $switch_file_hash . '\');switchInfo(\'' . $folderdocinfofile_id_initial . '\');
 </script>';
 }
 
@@ -104,7 +105,7 @@ if ($prev >= 0 && $prev < count($pages)) {
     
     $folderdocinfofile_id_prev = $get_folderdocid_prev->folderdocinfofile_id;
 
-    echo '<button class="btn" onclick="getPaging(' . $pages[$prev] . ');switchInfo(\'' . $folderdocinfofile_id_prev . '\');switchFile(\'LDF_1_2_6_ldf_09019588800598d8\');"><i class="fas fa-angle-left" aria-hidden="true" title="Previous"></i><span class="sr-only">Previous</span>  Previous</button>&nbsp;&nbsp;&nbsp;';
+    echo '<button class="btn" onclick="getPaging(' . $pages[$prev] . ');switchInfo(\'' . $folderdocinfofile_id_prev . '\');switchFile(\'' . $switch_file_hash . '\');"><i class="fas fa-angle-left" aria-hidden="true" title="Previous"></i><span class="sr-only">Previous</span>  Previous</button>&nbsp;&nbsp;&nbsp;';
         
 } else {
     echo '<button class="btn" disabled>Previous</button>&nbsp;&nbsp;&nbsp;';
@@ -119,21 +120,21 @@ if ($next >= 0 && $next < count($pages)) {
     
     $folderdocinfofile_id_next = $get_folderdocid_next->folderdocinfofile_id;    
     
-    echo '<button class="btn" onclick="getPaging(' . $pages[$next] . ');switchInfo(\'' . $folderdocinfofile_id_next . '\');switchFile(\'LDF_1_2_6_ldf_09019588800598d8\');">Next <i class="fas fa-angle-right" aria-hidden="true" title="Next"></i><span class="sr-only">Next</span></button>';
+    echo '<button class="btn" onclick="getPaging(' . $pages[$next] . ');switchInfo(\'' . $folderdocinfofile_id_next . '\');switchFile(\'' . $switch_file_hash . '\');">Next <i class="fas fa-angle-right" aria-hidden="true" title="Next"></i><span class="sr-only">Next</span></button>';
 
 if (($box_status == $box_validation_tag->term_id || $box_status == $box_rescan_tag->term_id) && !in_array($box_ticket_status, $rescan_validate_status_id_arr) && ($folderdocinfofile_id_validation != 1) && ($folderdocinfofile_id_rescan != 1)) {
-    echo '&nbsp;&nbsp;&nbsp;<button class="btn" id="next_validate" class="btn" onclick="executeValidation(\'' . $folderdocinfofile_id_current . '\');getPaging(' . $pages[$next] . ');switchInfo(\'' . $folderdocinfofile_id_next . '\');switchFile(\'LDF_1_2_6_ldf_09019588800598d8\');">Next + Validate <i class="fas fa-angle-double-right" aria-hidden="true" title="Next and Validate"></i><span class="sr-only">Next and Validate</span></button>';
+    echo '&nbsp;&nbsp;&nbsp;<button class="btn" id="next_validate" class="btn" onclick="executeValidation(\'' . $folderdocinfofile_id_current . '\');getPaging(' . $pages[$next] . ');switchInfo(\'' . $folderdocinfofile_id_next . '\');switchFile(\'' . $switch_file_hash . '\');">Next + Validate <i class="fas fa-angle-double-right" aria-hidden="true" title="Next and Validate"></i><span class="sr-only">Next and Validate</span></button>';
     //Rescan button
-    echo '&nbsp;&nbsp;&nbsp;<button class="btn" id="next_rescan" class="btn" onclick="executeRescan(\'' . $folderdocinfofile_id_current . '\');getPaging(' . $pages[$next] . ');switchInfo(\'' . $folderdocinfofile_id_next . '\');switchFile(\'LDF_1_2_6_ldf_09019588800598d8\');document.getElementById(\'paging\').contentWindow.location.reload();">Next + Re-Scan <i class="fas fa-angle-double-right" aria-hidden="true" title="Re-Scan"></i><span class="sr-only">Re-Scan</span></button>';
+    echo '&nbsp;&nbsp;&nbsp;<button class="btn" id="next_rescan" class="btn" onclick="executeRescan(\'' . $folderdocinfofile_id_current . '\');getPaging(' . $pages[$next] . ');switchInfo(\'' . $folderdocinfofile_id_next . '\');switchFile(\'' . $switch_file_hash . '\');document.getElementById(\'paging\').contentWindow.location.reload();">Next + Re-Scan <i class="fas fa-angle-double-right" aria-hidden="true" title="Re-Scan"></i><span class="sr-only">Re-Scan</span></button>';
 }
     
 } else {
     echo '<button class="btn" disabled>Next</button>';
     
 if (($box_status == $box_validation_tag->term_id || $box_status == $box_rescan_tag->term_id) && !in_array($box_ticket_status, $rescan_validate_status_id_arr) && ($folderdocinfofile_id_validation != 1) && ($folderdocinfofile_id_rescan != 1)) {
-    echo '&nbsp;&nbsp;&nbsp;<button class="btn" id="next_validate" onclick="executeValidation(\'' . $folderdocinfofile_id_current . '\');getPaging(' . $pages[$next] . ');switchInfo(\'' . $folderdocinfofile_id_next . '\');switchFile(\'LDF_1_2_6_ldf_09019588800598d8\');alert(\'' . $folderdocinfofile_id_current . ' has been validated\');location.reload();">Validate <i class="fas fa-check" aria-hidden="true" title="Validate Current"></i><span class="sr-only">Validate Current</span></button>';
+    echo '&nbsp;&nbsp;&nbsp;<button class="btn" id="next_validate" onclick="executeValidation(\'' . $folderdocinfofile_id_current . '\');getPaging(' . $pages[$next] . ');switchInfo(\'' . $folderdocinfofile_id_next . '\');switchFile(\'' . $switch_file_hash . '\');alert(\'' . $folderdocinfofile_id_current . ' has been validated\');location.reload();">Validate <i class="fas fa-check" aria-hidden="true" title="Validate Current"></i><span class="sr-only">Validate Current</span></button>';
     //Rescan button
-    echo '&nbsp;&nbsp;&nbsp;<button class="btn" id="next_rescan" class="btn" onclick="executeRescan(\'' . $folderdocinfofile_id_current . '\');getPaging(' . $pages[$next] . ');switchInfo(\'' . $folderdocinfofile_id_next . '\');switchFile(\'LDF_1_2_6_ldf_09019588800598d8\');alert(\'' . $folderdocinfofile_id_current . ' has been set to Re-Scan\');location.reload();">Re-Scan <i class="far fa-times-circle" aria-hidden="true" title="Re-Scan"></i><span class="sr-only">Re-Scan Current</span></button>';   
+    echo '&nbsp;&nbsp;&nbsp;<button class="btn" id="next_rescan" class="btn" onclick="executeRescan(\'' . $folderdocinfofile_id_current . '\');getPaging(' . $pages[$next] . ');switchInfo(\'' . $folderdocinfofile_id_next . '\');switchFile(\'' . $switch_file_hash . '\');alert(\'' . $folderdocinfofile_id_current . ' has been set to Re-Scan\');location.reload();">Re-Scan <i class="far fa-times-circle" aria-hidden="true" title="Re-Scan"></i><span class="sr-only">Re-Scan Current</span></button>';   
 }
     
 }
