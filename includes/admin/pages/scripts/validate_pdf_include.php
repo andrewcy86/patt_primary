de66ddcc600f321419e85e8f7020b2b75c2f39f6
Andrew Yuen <yuen.andrew@epa.gov>
2023-01-03 14:37:50 -0500

    PDF Fix

--- includes/admin/pages/scripts/validate_pdf_include.php
+++ includes/admin/pages/scripts/validate_pdf_include.php
@@ -34,25 +34,62 @@ $s3 = new Aws\S3\S3Client([
 	    'signature_version' => 'v4'
 	]);
 
-$file_key = 'f3b434cc86985e97520ee812d8877790';
-//$file_key = 'digitization-center/binary/0000040-2/0000040-2-02-1.pdf';
 
-$cmd = $s3->getCommand('GetObject', [
-    'Bucket' => bucket(),
-    //'Key' => 'nuxeodev/'.$file_key,
-    'Key' => $file_key,
-    #'ResponseContentDisposition' => 'inline; filename="'.$file_key.'.pdf"',
-    'ResponseContentDisposition' => 'inline',
-    'ResponseContentType' => 'application/pdf'
+    $curl = curl_init();
     
-]);
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://arms-dev-nuxeo.aws.epa.gov/nuxeo/api/v1/search/pp/nxql_search/execute?queryParams=select%20*%20from%20epa_record%20where%20arms:identifiers/*1/key=\'PATT%20ID\'%20AND%20arms:identifiers/*1/value/*1%20=\''.$obj_id.'\'',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+      CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json',
+        'X-NXproperties: *',
+        'Authorization: Basic c3ZjX2FybXNfcm06cGFzc3dvcmQ='
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    
+    $new_response = str_replace("file:content","file_content",$response);
+    
+    
+    curl_close($curl);
+    
+    $decoded = json_decode($new_response);
 
-$request = $s3->createPresignedRequest($cmd, '+20 minutes');
+    $child2exists = count($decoded->entries[0]->uid);
 
-$presignedUrl = (string)$request->getUri();
+    if ($child2exists == 1) {
+        
+        $uid = $decoded->entries[0]->uid;
+        $file_key = $decoded->entries[0]->properties->file_content->digest;
 
+        $cmd = $s3->getCommand('GetObject', [
+            'Bucket' => bucket(),
+            'Key' => 'nuxeodev/'.$file_key,
+            #'ResponseContentDisposition' => 'inline; filename="'.$file_key.'.pdf"',
+            'ResponseContentDisposition' => 'inline',
+            'ResponseContentType' => 'application/pdf'
+            
+        ]);
+        
+        $request = $s3->createPresignedRequest($cmd, '+20 minutes');
+        
+        $presignedUrl = (string)$request->getUri();
+?>
+<iframe src="<?php echo $presignedUrl; ?>" frameborder="0" scrolling="no" seamless="seamless" style="display:block; width:100%; height:100vh;"></iframe>
+<?php 
+
+    } else {
+        echo "PDF Not Found. Check ARMS";
+    }
 ?>
 
 <?php echo $obj_id; ?>
 
-<iframe src="<?php echo $presignedUrl; ?>" frameborder="0" scrolling="no" seamless="seamless" style="display:block; width:100%; height:100vh;"></iframe>
+
