8b3d35c64d30d1d0ad83c2eb4cbc7dbcb3ecbfa3
jemajor77 <32498207+jemajor77@users.noreply.github.com>
2024-02-27 15:23:19 -0500

    Datasync Map Run Cron Database Connection

--- includes/admin/patt_datasync_cron.php
+++ includes/admin/patt_datasync_cron.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $current_user, $wpscfunction, $wpdb;
 
-// Monitor the staus of the Datasync
+// Monitor the status of the Datasync
 $WP_PATH = implode("/", (explode("/", $_SERVER["PHP_SELF"], -2)));
 
 $dir = $_SERVER['DOCUMENT_ROOT'].$WP_PATH.'/app/mu-plugins/pattracking/includes/admin/pages/scripts';
@@ -47,6 +47,7 @@ $taskArn = 'arn:aws:datasync:us-east-1:114892021311:task/task-0f1bfec48faf20b0b'
 
 $datasync_status = '';
 
+
 try {
     $result = $dataSyncClient->describeTask([
         'TaskArn' => $taskArn,
@@ -75,45 +76,53 @@ try {
 
 
 // Execute State Machine/Step Function if status has changed from Running to Available
-// if($datasync_status == 'Available'){}
-$client = new Aws\Sts\StsClient([
-    'version'     => 'latest',
-    'region'  => region(),
-    'endpoint' => 'https://vpce-07a469e9e500866e6-wrptt4b4.sts.us-east-1.vpce.amazonaws.com'
-]);
+if($datasync_status == 'Available'){
+    $client = new Aws\Sts\StsClient([
+        'version'     => 'latest',
+        'region'  => region(),
+        'endpoint' => 'https://vpce-07a469e9e500866e6-wrptt4b4.sts.us-east-1.vpce.amazonaws.com'
+    ]);
 
-$ARN = "arn:aws:iam::114892021311:role/Customer-PATT-Datasync-Access";
-$sessionName = "AssumedRoleSession";
+    $ARN = "arn:aws:iam::114892021311:role/Customer-PATT-Datasync-Access";
+    $sessionName = "AssumedRoleSession";
 
-$new_role = $client->AssumeRole([
-    'RoleArn' => $ARN,
-    'RoleSessionName' => $sessionName,
-]);
+    $new_role = $client->AssumeRole([
+        'RoleArn' => $ARN,
+        'RoleSessionName' => $sessionName,
+    ]);
 
-// Initialize the DataSync client
+    // Initialize the DataSync client
 
-$sfnClient = new Aws\Sfn\SfnClient([
-    'version'     => 'latest',
-    'region'  => region(),
-    'credentials' =>  [
-        'key'    => $new_role['Credentials']['AccessKeyId'],
-        'secret' => $new_role['Credentials']['SecretAccessKey'],
-        'token'  => $new_role['Credentials']['SessionToken']
-    ]
-]);
+    $sfnClient = new Aws\Sfn\SfnClient([
+        'version'     => 'latest',
+        'region'  => region(),
+        'credentials' =>  [
+            'key'    => $new_role['Credentials']['AccessKeyId'],
+            'secret' => $new_role['Credentials']['SecretAccessKey'],
+            'token'  => $new_role['Credentials']['SessionToken']
+        ]
+    ]);
 
-// Specify the ARN of the Step Function
-$stateMachineArn = 'arn:aws:states:us-east-1:114892021311:stateMachine:MyStateMachine-hw0c9jta8';
+    // Specify the ARN of the Step Function
+    $stateMachineArn = 'arn:aws:states:us-east-1:114892021311:stateMachine:MyStateMachine-hw0c9jta8';
 
-$inputData = '{"Comment": "Executed"}';
+    $inputData = '{"Comment": "Executed"}';
 
-$result = $sfnClient->startExecution([
-    'stateMachineArn' => $stateMachineArn,
-    'input'           => $inputData,
-]);
+    $result = $sfnClient->startExecution([
+        'stateMachineArn' => $stateMachineArn,
+        'input'           => $inputData,
+    ]);
 
-echo 'Execution ARN: ' . $result['executionArn'];
+    echo 'Execution ARN: ' . $result['executionArn'];
 
+    $epa_datasync_status_table = $wpdb->prefix . 'epa_datasync_status';
 
+	// POPULATING Datasync Status Table
+
+    $wpdb->insert($epa_datasync_status_table, array(
+    'execution_arn_id' => $result['executionArn'];,
+    'status' => $datasync_status ));
+
+}
 
 ?>
