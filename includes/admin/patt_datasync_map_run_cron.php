c1e1625cc8095f8edc1b3fa8214738016e97da97
Garner <Garner.Kiwane@epa.gov>
2024-03-13 17:26:55 -0400

    corrected the datasync cron file paths

--- includes/admin/patt_datasync_map_run_cron.php
+++ includes/admin/patt_datasync_map_run_cron.php
@@ -13,9 +13,8 @@ use Aws\RoboMaker\RoboMakerClient;
 
 global $wpdb, $current_user, $wpscfunction;
 
-$WP_PATH = implode("/", (explode("/", $_SERVER["PHP_SELF"], -1)));
 
-$dir = $_SERVER['DOCUMENT_ROOT'].$WP_PATH.'/pages/scripts';
+$dir = $_SERVER['DOCUMENT_ROOT'].'/app/mu-plugins/pattracking/includes/admin/pages/scripts';
 
 require_once($dir."/vendor/autoload.php");
 
